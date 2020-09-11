340088db5f1dab9f9e99d4ecb3b380d05bdea6d2
nikhil12894 <nikhil12894@gmail.com>
2020-09-12 00:45:26 +0530

    haroku db config

--- src/main/java/com/example/bootdemo/RestApp.java
+++ src/main/java/com/example/bootdemo/RestApp.java
@@ -1,5 +1,10 @@
 package com.example.bootdemo;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +14,12 @@ public class RestApp {
 
 	@GetMapping(path = "test")
 	@RequestMapping
-	public String greet() {
-		return "Hello World";
+	public List<Map<String, Object>> greet() {
+		List<Map<String, Object>> returnMap = jdbcTemplate.queryForList("SELECT * FROM roles");
+		return returnMap;
 	}
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
 }
